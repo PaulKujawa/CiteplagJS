bdea02a13e7b61d7511df3ba6526aabbe0e8e539
Paul Kujawa <p.kujawa@gmx.net>
2014-11-11 15:02:41 +0100

    can read xml file with jquery

--- js/controllers/upload_controller.js
+++ js/controllers/upload_controller.js
@@ -1,22 +1,29 @@
 Uploads.UploadController = Ember.ArrayController.extend({
     actions: {
-        createFile: function(){
+        uploadFiles: function(){
+            $.ajax({
+                type: "GET" ,
+                url: "collusion.xml" ,
+                dataType: "xml",
+                contentType: "text/xml;charset=utf-8",
+                async: false,
 
-            alert("ok");
-            /*
-            var fd = new FormData(document.getElementById("fileinfo"));
-            fd.append("postContent", this.get('newPost'));
-            this.set('newPost', ''); //reset text field
-            $('#inputFile').val(''); //reset fileinput field
+                success: function(file) {
+                    var xml = $(file);
+                    var title = xml.find("title");
+                    console.log( title.text() );
 
-            Ember.$.ajax({
-                url: "http://localhost:3000/posts",
-                type: "POST",
-                data: fd,
-                processData: false,  // tell jQuery not to process the data
-                contentType: false,   // tell jQuery not to set contentType
+                    //var xmlDoc = $.parseXML(xml);
+                    //$(xmlDoc).find('title').each(function() {
+                    //   alert ( $(this).text() );
+                    //});
+                },
+
+                error: function(xhr, textStatus, error) {
+                    console.log( textStatus + ': ' + xhr.responseText);
+                    showError('error:' + xhr.status);
+                }
             });
-            */
         }
     }
 });
