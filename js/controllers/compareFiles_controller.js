b118f9245aad487410f7cbbde8f359d51d32f882
Paul Kujawa <p.kujawa@gmx.net>
2014-11-18 21:33:28 +0100

    parsing one or n matches in collusion.xml

--- js/controllers/compareFiles_controller.js
+++ js/controllers/compareFiles_controller.js
@@ -1,34 +1,37 @@
 App.CompareFilesController = Ember.ArrayController.extend({
     actions: {
         upload: function() {
-            store = this.store;
+            var store = this.store;
 
             $.ajax({
                 type: "GET" ,
-                url: "xmlFiles/" + "collusion.xml",// + this.get('suspiciousFile'),
+                url: "xmlFiles/collusion.xml",// + this.get('suspiciousFile'),
                 dataType: "xml",
 
                 success: function(file) { // for collusion controller
                     var parsedJSON = $.xml2json(file);
 
-                    $.each(parsedJSON.alignments.match, function(i, vMatch) { // o(n)
-                        if (typeof  vMatch.detail !== undefined) {/* todo */}
+                    if (parsedJSON.alignments.match.ref === undefined)
+                        var matches = parsedJSON.alignments.match; // matches
+                    else
+                        var matches = parsedJSON.alignments; // 1 match
 
-                        // align, match, was mit details machen?
-                        // 1. doc, f_pattern1 > start & length nur placeholder?
-                        // 2. doc, f_pattern1 > kein start & length?
+
+
+                    $.each(matches, function(i, vMatch) { // o(n)
+                        if (vMatch.detail !== undefined) {/* todo */}
 
                         $.each(vMatch.ref, function(j, vRef) { // o(2)
+
                             $.each(parsedJSON.document, function(k, vDoc) { // o(2)
                                 if (vDoc.id === vRef.document) {
+
                                     $.each(vDoc.feature, function(h, vFeat) { // o(n)
-                                        if (vFeat.id === vRef.feature) {
-                                            if (vFeat.start)
-                                                var until = parseInt(vFeat.start) + parseInt(vFeat.length);
-                                                console.log(
-                                                    vFeat.type + ": " +
-                                                    vFeat.start + " - " + until
-                                                );
+                                        if (vFeat.id === vRef.feature && vFeat.start !== undefined) {
+                                            var until = parseInt(vFeat.start) + parseInt(vFeat.length);
+                                            console.log(
+                                                vFeat.type + ": " + vFeat.start + " - " + until
+                                            );
                                         }
                                     });
                                 }
@@ -36,12 +39,9 @@ App.CompareFilesController = Ember.ArrayController.extend({
                         });
                     });
 
-
-
                     /*store.createRecord('compareFiles', {
                        title: part
                     });*/
-
                 },
 
                 error: function(xhr, textStatus, error) {
