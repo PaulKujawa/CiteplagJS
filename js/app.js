f417bfd6d1f64e510459c906326fc4110c181337
Paul Kujawa <p.kujawa@gmx.net>
2014-11-19 12:14:29 +0100

    first file content displayed

--- js/app.js
+++ js/app.js
@@ -1,57 +1,50 @@
 $(function() {
     const xmlFolder = "xmlFiles/";
+    var collusionJSON, compareFiles = new Object(),
+        getSourceFile, parseFiles, getXMLFile, readXML;
 
-    var getXMLFile = function(filename) {
+    $('#collusionFileInput').change(function() {
+        var filename = $(this).val();
+        getXMLFile(filename, function(file) {
+            collusionJSON = $.xml2json(file);
+            getSourceFile(0);
+        });
+    });
+
+    getSourceFile = function(i) {
+        var doc = collusionJSON.document[i];
+
         $.ajax({
             type: "GET",
-            url: xmlFolder + filename,
+            url: xmlFolder + doc.src,
             dataType: "xml",
             success: function(file) {
-                return file;
+                compareFiles[doc.id] = (new XMLSerializer()).serializeToString(file);
+                if (i === 0)
+                    getSourceFile(1);
+                else
+                    parseFiles();
             },
             error: function(xhr, textStatus, error) {
                 console.log( [textStatus, xhr.responseText].join(':') );
             }
-        })
-    };
-
-
-    var getCompareFiles = function(parsedJSON) {
-        var xmlFiles = {};
-        $.each(parsedJSON.document, function(i, doc) {
-            var file    = getXMLFile(doc.src),
-                content = (new XMLSerializer()).serializeToString(xmlFolder+file);
-            xmlFiles.push(doc.id, content);
         });
-        return xmlFiles;
     };
 
 
-    var readXML = function(content, from, length) {
-        from = parseInt(from);
-        length = parseInt(length);
-        return content.substring(from, from+length);
-    };
-
-
-    $('#collusionFileInput').change(function() {
-        var filename    = $(this).val(),
-            xmlFile     = getXMLFile(filename),
-            parsedJSON  = $.xml2json(xmlFile),
-            compFiles   = getCompareFiles(parsedJSON),
-            matches     = parsedJSON.alignments; // just one match
-
-        if (parsedJSON.alignments.match.ref === undefined)
-            matches = parsedJSON.alignments.match; // matches
+    parseFiles = function() {
+        var matches = collusionJSON.alignments; // just one match
+        if (collusionJSON.alignments.match.ref === undefined)
+            matches = collusionJSON.alignments.match; // matches
 
         $.each(matches, function(i, vMatch) {
             if (vMatch.detail !== undefined) {/*todo*/}
             $.each(vMatch.ref, function(j, vRef) {
-                $.each(parsedJSON.document, function(k, vDoc) {
+                $.each(collusionJSON.document, function(k, vDoc) {
                     if (vDoc.id === vRef.document) {
                         $.each(vDoc.feature, function(h, vFeat) {
                             if (vFeat.id === vRef.feature) {
-                                var content = compFiles[doc.id];
+                                var content = compareFiles[vDoc.id];
                                 var excerpt = readXML(content, vFeat.start, vFeat.length);
                                 $('#suspiciousFileArea').append(excerpt);
                             }
@@ -60,5 +53,24 @@ $(function() {
                 });
             });
         });
-    });
+    };
+
+
+    getXMLFile = function(filename, callback) {
+        $.ajax({
+            type: "GET",
+            url: xmlFolder + filename,
+            dataType: "xml",
+            success: callback,
+            error: function(xhr, textStatus, error) {
+                console.log( [textStatus, xhr.responseText].join(':') );
+            }
+        })
+    };
+
+    readXML = function(content, from, length) {
+        from = parseInt(from);
+        length = parseInt(length);
+        return content.substring(from, from+length);
+    };
 });
