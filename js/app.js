09f03515ce222e2d11a301a55ca8abec72a1ceaf
Paul Kujawa <p.kujawa@gmx.net>
2014-11-26 23:06:18 +0100

    parser works!

--- js/app.js
+++ js/app.js
@@ -102,6 +102,7 @@ $(function() {
         $.each(matchTypes, function(matchTitle, mType) { //mType[m][d]['start']
             var featurePositions    = orderFeaturePos(mType, 0),
                 leftFileHTML        = convertXMLtoHTML(compareFilesXML[0], featurePositions);
+
             featurePositions        = orderFeaturePos(mType, 1);
             var rightFileHTML       = convertXMLtoHTML(compareFilesXML[1], featurePositions);
 
@@ -130,7 +131,7 @@ $(function() {
 
 
     convertXMLtoHTML = function(xmlString, featurePositions) { //  todo what to do with features?!
-       /* var highPos = xmlString.length-1,
+        var highPos = xmlString.length-1,
             lowPos  = highPos;
 
         while(lowPos !== 0) {
@@ -141,73 +142,41 @@ $(function() {
                 featurePositions.splice(0, 1); // removes 1 item from index 0
             }
 
-            var excerpt         = xmlString.substr(lowPos, highPos-lowPos),
-                replacedExcerpt = convertBetweenFeatures(excerpt);
+            var excerpt = xmlString.substr(lowPos, highPos-lowPos+1);
+            xmlString   = xmlString.substr(0, lowPos) + convertBetweenFeatures(excerpt) +
+                          xmlString.substr(highPos+1, xmlString.length-highPos-1);
+            highPos = lowPos-1;
         }
-        */
-
-            var replacedExcerpt = convertBetweenFeatures(xmlString);
-
-            xmlString = xmlString.replace(xmlString, replacedExcerpt);
-//            highPos = lowPos-1;
-
         return xmlString;
     };
 
 
     convertBetweenFeatures = function(excerpt) {
-        console.log( excerpt);
-        console.log( );
         var closingPos = null;
+
         for(var highPos = excerpt.length-1; highPos >= 0; highPos--) {
-            if (excerpt[highPos] === '>')
+            if (excerpt[highPos] === '>') {
                 closingPos = highPos;
 
-            else if (excerpt[highPos] === '<') {
-                if (closingPos === null)
-                    alert("error: one xml tag itself isn't closed");
-                else {
-                    var toReplace = excerpt.substr(highPos, closingPos-highPos+1);
+            } else if (excerpt[highPos] === '<') {
+                if (excerpt[highPos+1] === '/') {
+                    excerpt     = excerpt.substr(0, highPos) + '</div>' +
+                                  excerpt.substr(closingPos+1, excerpt.length-closingPos-1);
 
-                    if (excerpt[highPos+1] === '/')
-                        excerpt = excerpt.replace(toReplace, '</div>');
+                } else {
+                    var toReplace = excerpt.substr(highPos, closingPos-highPos+1),
+                        length = toReplace.indexOf(' '); // would cut off attr
+                    if (length == -1)
+                        length = closingPos-highPos;
 
-                    else {
-                        var length = toReplace.indexOf(' ');
-                        if (length == -1) // xmlTag with attr, which don't matter
-                            length = closingPos-highPos;
-
-                        var xmlTag = excerpt.substr(highPos+1, length-1);
-                        console.log(
-                            "toReplace: " + toReplace +
-                            " length: " + length +
-                            " xmlTag: " + xmlTag
-                        );
-
-                        excerpt = excerpt.replace(toReplace, '<div class="'+xmlTag+'">');
-                    }
+                    var xmlTag  = excerpt.substr(highPos+1, length-1);
+                    excerpt     = excerpt.substr(0, highPos) + '<div class="'+xmlTag+'">' +
+                                  excerpt.substr(closingPos+1, excerpt.length-closingPos-1);
                 }
             }
         }
-        console.log( excerpt );
         return excerpt;
     };
-
-    var str = '<p>The traditional consultation in general practice was brief.<xref ref-type="bibr" ' +
-        'rid="B2">2</xref> The patient presented symptoms and the doctor prescribed treatment. In 1957 ' +
-        'Balint gave new insights into the meaning of symptoms.<xref ref-type="bibr" rid="B10">10</xref> ' +
-        'By 1979 an enhanced model of consultation was presented, in which the doctors dealt with ongoing ' +
-        'as well as presenting problems and added health promotion and education about future appropriate ' +
-        'use of services.<xref ref-type="bibr" rid="B11">11</xref> Now, with an ageing population and more ' +
-        'community care of chronic illness, there are more issues to be considered at each consultation. ' +
-        'Ideas of what constitutes good general practice are more complex.' +
-        '<xref ref-type="bibr" rid="B12">12</xref> Good practice now includes both extended care ' +
-        'of chronic medical problems&#x2014;for example, coronary heart disease' +
-        '<xref ref-type="bibr" rid="B13">13</xref>&#x2014;and a public health role. At first this ' +
-        'model was restricted to those who lead change (&#x201C;early adopters&#x201D;) and ' +
-        'enthusiasts<xref ref-type="bibr" rid="B14">14</xref> but now it is embedded in professional ' +
-        'and managerial expectations of good practice.</p>';
-    convertBetweenFeatures(str);
 
 
     createTab = function(patternTitle, leftFileHTML, rightFileHTML) {
